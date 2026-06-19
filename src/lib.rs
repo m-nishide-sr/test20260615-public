730a96e39fc7e6ceaaff2f22d82988863fe9883a
m-nishide-sr <292954160+m-nishide-sr@users.noreply.github.com>
2026-06-19 01:00:29 +0000

    フィボナッチ関数の実装とベンチマークの追加、Cargo設定の更新

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod fibonacci_async;
+mod fibonacci;
+
+pub use fibonacci::{fibonacci, fibonacci_loop}; 
+pub use fibonacci_async::fibonacci_async; 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use hello_cargo::{fibonacci,fibonacci_loop,fibonacci_async}; // 👈 ライブラリからインポート
+
+#[tokio::main]
+async fn main() {
+   println!("Hello, world!");
+   println!("fibonacci_async(30):{}", fibonacci_loop(30));
+   println!("fibonacci_async(30):{}", fibonacci(30));
+   println!("fibonacci_async(30):{}", fibonacci_async(30).await);
 }
